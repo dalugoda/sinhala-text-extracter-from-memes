a61bd8e0846d2d2e26fc7808a6745aaf61305b90
Sandupa Dalugoda <sandupasanthusha@gmail.com>
2019-02-01 17:30:45 +0530

    data annotate script

---
+++ data-annotate.py
@@ -0,0 +1,29 @@
+import cv2
+import os
+import sys
+import shutil
+import time
+
+segmented_dir = 'annotate_test/segmented/meme_01'
+data_set_dir = 'annotate_test/cnn_dataset/'
+
+character_list = []
+for (dir_path, dir_names, file_names) in os.walk(segmented_dir):
+    character_list.extend(file_names)
+    break
+
+for char in character_list:
+    char_path = segmented_dir + '/' + char
+    img = cv2.imread(char_path)
+    cv2.imshow(char, img)
+    cv2.waitKey(2)
+
+    label = input("Enter Label for " + char + " : ")
+    cv2.destroyWindow(char)
+
+    label_path = data_set_dir + str(label)
+    if not os.path.exists(label_path):
+        os.makedirs(label_path)
+
+    timestamp = str(time.time()).split('.')[0]
+    shutil.move(char_path, label_path + '/' + timestamp + char)
